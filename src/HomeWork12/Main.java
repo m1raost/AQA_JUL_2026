b1119ffbe9a385942d7e9bc5b6e6a24c098f40a0
m1raost <myroslavaostapenko0>
2026-09-10 13:26:25 +0200

    HomeWork12(2)

--- src/HomeWork12/Main.java
+++ src/HomeWork12/Main.java
@@ -1,35 +1,32 @@
 package HomeWork12;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Map<IPhone, List<String>> phoneContacts = new HashMap<>();
+        Map<IPhone, Set<String>> phoneContacts = new HashMap<>();
 
         Android android1 = new Android("White", "Nokia");
         Apple apple1 = new Apple("Golden", "14 Pro");
 
-        phoneContacts.put(android1, new ArrayList<>());
+        phoneContacts.put(android1, new HashSet<>());
         phoneContacts.get(android1).add("123-456-789");
         phoneContacts.get(android1).add("333-222-111");
 
-        phoneContacts.put(apple1, new ArrayList<>());
+        phoneContacts.put(apple1, new HashSet<>());
         phoneContacts.get(apple1).add("380-272-736");
         phoneContacts.get(apple1).add("163-826-385");
 
         System.out.println(phoneContacts);
 
-        Map<String, List<IPhone>> ownerPhones = new HashMap<>();
-        ownerPhones.put("Alice", new ArrayList<>());
+        Map<String, Set<IPhone>> ownerPhones = new HashMap<>();
+        ownerPhones.put("Alice", new HashSet<>());
         ownerPhones.get("Alice").add(android1);
         ownerPhones.get("Alice").add(apple1);
 
-        ownerPhones.put("Bob", new ArrayList<>());
+        ownerPhones.put("Bob", new HashSet<>());
         Android android2 = new Android("Red", "Oppo");
         ownerPhones.get("Bob").add(android2);
 
